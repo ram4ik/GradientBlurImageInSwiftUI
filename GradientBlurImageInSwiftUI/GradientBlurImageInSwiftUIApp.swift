0868d1f0b5b57e0ccb4fa12757a7849f3197f232
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-08-03 11:30:48 +0300

    Initial Commit

---
+++ GradientBlurImageInSwiftUI/GradientBlurImageInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientBlurImageInSwiftUIApp.swift
+//  GradientBlurImageInSwiftUI
+//
+//  Created by ramil on 03.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct GradientBlurImageInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
