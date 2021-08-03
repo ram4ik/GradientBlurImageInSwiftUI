32485051972143db22d8ac29c235efa329c650bd
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-08-03 11:41:30 +0300

    init

--- GradientBlurImageInSwiftUI/ContentView.swift
+++ GradientBlurImageInSwiftUI/ContentView.swift
@@ -9,8 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("cat")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 400)
+            Blur(style: .dark)
+                .frame(height: 400)
+            Image("cat")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 400)
+                .mask(
+                    LinearGradient(gradient: Gradient(colors: [
+                        Color.white.opacity(0.1),
+                        Color.white,
+                        Color.white.opacity(0.1)
+                        
+                    ]), startPoint: .top, endPoint: .bottom)
+                )
+        }
+    }
+}
+
+struct Blur: UIViewRepresentable {
+    var style: UIBlurEffect.Style = .systemMaterial
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
     }
 }
 
